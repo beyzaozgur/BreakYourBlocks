1ed82033d2ab99e64a239414010584f1e2d5381d
beyzaozgur <beyzaozgur35@hotmail.com>
2023-05-11 23:22:36 +0300

    add user test prediction results to firebase collection

--- app/api/model/load_model_and_predict.py
+++ app/api/model/load_model_and_predict.py
@@ -2,10 +2,11 @@ import os
 import tensorflow as tf
 import numpy as np
 import pandas as pd
+from firebase_admin import firestore
 
 # prediction_data_dir = 'C:/Users/w/StutteringDetectionModel/MultiLabelClassification/multi-label-test-data/'
 
-def load_model_and_predict(prediction_data_dir):
+def load_model_and_predict(prediction_data_dir, testId, userId, testCompletitionTime):
     images_array = []  
     for i in os.listdir(prediction_data_dir):
         img = tf.keras.utils.load_img(prediction_data_dir + i, target_size=(200, 200, 3))
@@ -33,3 +34,25 @@ def load_model_and_predict(prediction_data_dir):
 
     # _, acc = model.evaluate(model.X_test, model.y_test)
     # print("Accuracy = ", (acc * 100.0), "%")
+
+    data = {
+        'userID': userId,
+        'testID': testId,
+        'testDate': testCompletitionTime,
+        str(classes[sorted_categories[0]]) : str(proba[0][sorted_categories[0]]),
+        str(classes[sorted_categories[1]]) : str(proba[0][sorted_categories[1]]),
+        str(classes[sorted_categories[2]]) : str(proba[0][sorted_categories[2]]),
+        str(classes[sorted_categories[3]]) : str(proba[0][sorted_categories[3]]),
+        str(classes[sorted_categories[4]]) : str(proba[0][sorted_categories[4]]),
+        str(classes[sorted_categories[5]]) : str(proba[0][sorted_categories[5]]),
+        str(classes[sorted_categories[6]]) : str(proba[0][sorted_categories[6]]),
+        str(classes[sorted_categories[7]]) : str(proba[0][sorted_categories[7]]),
+        str(classes[sorted_categories[8]]) : str(proba[0][sorted_categories[8]]),
+        str(classes[sorted_categories[9]]) : str(proba[0][sorted_categories[9]])
+    }
+
+    db = firestore.client()
+    # Add a new doc in collection 'cities' with ID 'LA'
+    db.collection(u'userTestResults').add(data)
+
+    print("COLLECTION ADDED...")
